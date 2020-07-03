02288ac3a6fd077471ed2acadb80899c0f5f0673
zettabyte.kim@gmail.com <Zetta Kim>
2020-07-04 06:24:05 +0900

    7/4 commit

--- httpPost/main.go
+++ httpPost/main.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"bytes"
+	"encoding/xml"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 )
 
+// Person struct
+type Person struct {
+	Name string
+	Age  int
+}
+
+func xmlPost() {
+	person := Person{"Alex", 10}
+	pbytes, _ := xml.Marshal(person)
+	buff := bytes.NewBuffer(pbytes)
+	req, err := http.NewRequest("POST", "http://httpbin.org/post", buff)
+	check(err)
+	req.Header.Add("Context-Type", "application/xml")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	check(err)
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err == nil {
+		str := string(respBody)
+		println(str)
+	}
+}
 func httpPostForm() {
 	resp, err := http.PostForm("http://httpbin.org/post", url.Values{"Name": {"Lee"}, "Age": {"10"}})
 	check(err)
@@ -41,4 +67,5 @@ func check(e error) {
 func main() {
 	httpPost()
 	httpPostForm()
+	xmlPost()
 }
